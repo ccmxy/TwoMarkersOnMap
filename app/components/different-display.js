12f59cdfbb72a4aadb7eb35a43a3711519a9713b
ccmxy <windrainicefire@gmail.com>
2015-10-09 20:28:35 -0700

    partly added show map functionality

--- app/components/different-display.js
+++ app/components/different-display.js
@@ -4,19 +4,29 @@ export default Ember.Component.extend({
   showList: true,
   showThumbnail: false,
   showGallery: false,
+  showMap: false,
   actions:{
     thumbnailFormShow() {
       this.set('showGallery', false),
       this.set('showList', false),
-      this.set('showThumbnail', true);
+      this.set('showThumbnail', true),
+      this.set('showMap', false);
     },
     listFormShow() {
       this.set('showGallery', false),
       this.set('showList', true),
-      this.set('showThumbnail', false);
+      this.set('showThumbnail', false),
+      this.set('showMap', false);
     },
     galleryFormShow() {
       this.set('showGallery', true),
+      this.set('showList', false),
+      this.set('showThumbnail', false),
+      this.set('showMap', false);
+    },
+    mapsFormShow() {
+      this.set('showMap', true),
+      this.set('showGallery', false),
       this.set('showList', false),
       this.set('showThumbnail', false);
     },
